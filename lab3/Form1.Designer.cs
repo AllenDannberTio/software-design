59ebcfc02c81545f3b5bc44523ead49fb4822141
AllenDannberTio <a.tio.546575@umindanao.edu,ph>
2025-04-02 20:37:33 +0800

    Week 3 Finished

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,52 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnShowInfo = new Button();
+            listBoxBooks = new ListBox();
+            SuspendLayout();
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(257, 355);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(339, 63);
+            btnShowInfo.TabIndex = 0;
+            btnShowInfo.Text = "Show Information";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(133, 54);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(573, 264);
+            listBoxBooks.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBoxBooks);
+            Controls.Add(btnShowInfo);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnShowInfo;
+        private ListBox listBoxBooks;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,81 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} - Issue {IssueNumber}";
+            }
+        }
+
+        public class Ebook : Book
+        {
+            public string FileFormat { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} (Ebook in {FileFormat} format)";
+            }
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book[] books =
+                        {
+                new Book { Title = "C# In a Nutshell", Author = "John Doe" },
+                new Magazine { Title = "Tech Weekly", Author = "Joe Mama", IssueNumber = 77 },
+                new Ebook { Title = "C# Residuals", Author = "Chris Brown", FileFormat = "PDF" },
+                new Textbook { Title = "We Don't Trust You", Author = "Dr. Future", Subject = "MetroBoomin" },
+                new AudioBook { Title = "The Perfect Luv Tape", Author = "Lil Uzi Vert", Narrator = "Playboi Carti", Duration = 3.5 }
+          
+
+
+        };
+            DisplayBooksInfo(books);
+        }
+
+        public void DisplayBooksInfo(Book[] books)
+        {
+            listBoxBooks.Items.Clear();
+            foreach (var book in books)
+            {
+                listBoxBooks.Items.Add(book.GetInfo());
+            }
+        }
+
+        public class Textbook : Book
+        {
+            public string Subject { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} (Textbook on {Subject}) by {Author}";
+            }
+        }
+
+        public class AudioBook : Book
+        {
+            public string Narrator { get; set; }
+            public double Duration { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} (Audiobook, Narrated by {Narrator}, {Duration} hours)";
+            }
+        }
+
+    }
 }
